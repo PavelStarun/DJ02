128198f8f169f6863dea92a36cf0fbecc6c1e2f8
PavelStarun <starunpavelpython@gmail.com>
2024-07-24 20:25:58 +0300

    3

--- main/appDJ02/urls.py
+++ main/appDJ02/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('basics', views.basics, name='basics'),
-    path('equipment', views.equipment, name='equipment'),
-    path('tips', views.tips, name='tips'),
-    path('safety', views.safety, name='safety'),
+    path('basics/', views.basics, name='basics'),
+    path('equipment/', views.equipment, name='equipment'),
+    path('tips/', views.tips, name='tips'),
+    path('safety/', views.safety, name='safety'),
 ]

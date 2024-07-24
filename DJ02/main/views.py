3ea44925e8b06966ab99f8c2fc9ac191d02f6341
PavelStarun <starunpavelpython@gmail.com>
2024-07-24 21:31:23 +0300

    5

--- DJ02/main/views.py
+++ DJ02/main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+def basics(request):
+    return render(request, 'main/basics.html')
+
+def equipment(request):
+    return render(request, 'main/equipment.html')
+
+def tips(request):
+    return render(request, 'main/tips.html')
+
+def safety(request):
+    return render(request, 'main/safety.html')

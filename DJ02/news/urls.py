913c5a928ab53e42f3a0ca8fba7b3aab887133c2
PavelStarun <starunpavelpython@gmail.com>
2024-07-28 21:16:26 +0300

    7

--- DJ02/news/urls.py
+++ DJ02/news/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.news, name='news'),
-    path('<int:news_id>/', views.news_detail, name='news_detail'),
-    #path('<int:pk>/', views.news_detail, name='news_detail'),
+    path('<int:pk>/', views.news_detail, name='news_detail'),
 ]
